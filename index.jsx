c49cd5445c70b18f3d9c894bda75c6509e57db7d
duyndd <nguyendinhd789@gmail.com>
2024-03-04 20:18:41 +0700

    init project

---
+++ index.jsx
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Đình Duy</title>
+		<meta name="robots" content="noindex" />
+
+		<link rel="stylesheet" href="./css/styles.css" />
+	</head>
+
+	<body>
+		<div class="section-content" style="font-size: 10pt;"></div>
+		<div class="cv-document">
+			<div class="cv-page">
+				<div class="section" id="info">
+					<h1 style="color:#0e2452;">Nguyễn Đình Duy</h1>
+					<div class="section-content"></div>
+					<p><b>Ngày sinh</b> <span style="margin-right: 20px;"></span>09/04/2004</p>
+                    <p><b>Giới tính</b> <span style="margin-right: 27px;"></span>Nam</p>
+					<p><b>Điện thoại</b> <span style="margin-right: 20px;"></span>(+84) 789492494</p>
+					<p><b>Email</b> <span style="margin-right: 45px;"></span>duyndde180753@fpt.edu.vn</p>
+					<p><b>Địa chỉ</b> <span style="margin-right: 35px;"></span>Đà Nẵng , Việt Nam</p>
+					<p><b>Website</b> <span style="margin-right: 28px;"></span> Đình Duy Website</p>
+                    <div class="avatar"><img src="./images/385481296_1361457598114789_2604014554430833443_n.jpg" width="250" style="position: absolute; top: 10px; right: 5%; margin: 20px;" alt=""></div>
+									
+				</div>				
+
+			<div class="section" id="target">
+					<h2 class="section-title">MỤC TIÊU NGHỀ NGHIỆP</h2>		
+					<style>
+						h2 {
+							border-bottom: 2px solid black;
+						}
+					</style>				
+					<p> Là một sinh viên đang học chương trình ở FPT University. Em mong muốn được làm việc với vị trí Fullstack Developer, có thể vận dụng những kiến thức đã được trau dồi và không ngừng học hỏi để phát triển bản thân.</p>		
+				</div>
+
+				<div class="section" id="education">
+					<h2 class="section-title">HỌC VẤN</h2>
+					<div class="section-content"></div>		
+					<ul>
+					<li><p>11/2022 <span style="margin-right: 28px;"></span> Bắt đầu chương trình học tại FPT University.</p></li>
+					<hr/>
+					<li><p>01/2024-02/2024 <span style="margin-right: 28px;"></span> Học chuyên ngành Software Engineer tại FPT University.</p></li>                    
+					</ul>
+				</div>
+
+				<div class="section" id="experience">
+					<h2 class="section-title">KINH NGHIỆM LÀM VIỆC</h2>
+					<div class="section-content"></div>
+					<ul>
+					<li> <p>02/2024-Hiện tại <span style="margin-right: 30px;"></span> Thực hiện các dự án Website theo yêu cầu và thiết kế sẵn có.
+						Thiết kế và phân tích Database, sử dụng các hệ quản trị CSDL khác nhau (MySQL, SQLServer, NoSQL, …) và lập trình các câu truy vấn.
+						Lập trình các Web Server bằng các ngôn ngữ server-side (C, Java, …).</p></li>
+					</ul>
+				</div>
+
+				<div class="section" id="activity">
+					<h2 class="section-title">HOẠT ĐỘNG</h2>
+					<div class="section-content"></div>
+					<p> 02/2023-02/2024 <span style="margin-right: 64px;"></span> Tham gia các hoạt động của trường. </p>
+					<p> 02/2023-07/2023 <span style="margin-right: 64px;"></span> Tham gia các câu lạc bô Code. </p>
+
+				</div>
+
+				<div class="section" id="skill">
+					<h2 class="section-title">KỸ NĂNG</h2>
+					<div class="section-content"></div>
+					<p><b>HTML/CSS</b> <span style="margin-right: 106px;"></span>Fundamental</p>
+					<hr/>
+					<p><b>C</b> <span style="margin-right: 164px;"></span>Fundamental</p>
+				</div>
+
+				<div class="section" id="certificate">
+					<h2 class="section-title">CHỨNG NHẬN - GIẢI THƯỞNG</h2>
+					<div class="section-content"></div>
+                    <p> 2023 <span style="margin-right: 147px;"></span> Lập trình website. </p>
+				</div>
+
+				<div class="section" id="hobby">
+					<h2 class="section-title">SỞ THÍCH</h2>
+					<div class="section-content"></div>
+					<ul>
+					<li><p>Đọc sách</p></li>
+					<li><p>Nghe nhạc</p></li>
+					<li><p>Chơi game</p></li>
+					<li><p>Xem phim</p></li>
+					</ul>
+				</div>
+
+				<div class="section" id="project">
+					<h2 class="section-title">DỰ ÁN</h2>	
+					<div class="section-content"></div>
+					<h3>WEBSITE BÁN HÀNG TRỰC TUYẾN</h3>
+					<p>2023</p>
+					<ul>
+					<li><b>Vị trí:</b>Developer</li>
+					<li><b>Mô tả:</b>Tham gia phát triển một ứng dụng web bán hàng trực tuyến cho công ty thương mại điện tử lớn.</li>
+		            <li><b>Nhiệm vụ:</b>Thiết kế, lập trình, kiểm thử, và bảo trì các chức năng của ứng dụng web, bao gồm giao diện người dùng, xử lý dữ liệu, tích hợp thanh toán, và tối ưu hóa hiệu năng.</li>
+					<li><b>Công nghệ:</b>HTML,CSS, JavaScript.</li>
+					</ul>
+				</div>
+
+
+				<div class="section" id="contact">
+					<h2 class="section-title">Liên hệ</h2>
+					<ol>
+						<li><a href="https://www.facebook.com/profile.php?id=100027516857250">Facebook</a></li>
+						<li><a href="https://www.instagram.com/_iam.zuyy_/">Instagram</a></li>
+					  </ol>
+					  
+					<div class="section-content"></div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
